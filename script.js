874e4488a0cf7cdfd5426503a515452d27d16892
Muizah <mapampa7@gmail.com>
2022-06-02 19:45:23 +0100

    working on mobile responsiveness

--- script.js
+++ script.js
@@ -15,3 +15,27 @@ inputWrapperInput.addEventListener("keyup", InputTextChange);
 function InputTextChange() {
   inputWrapper.style.borderColor = "hsl(0, 36%, 70%)";
 }
+var heroImage = document.querySelector("#heroImage");
+changeHeroByScreenSize();
+function changeHeroByScreenSize() {
+  var mobileImage = "/base-apparel-coming-soon-master/images/hero-mobile.jpg";
+  var desktopImage = "/base-apparel-coming-soon-master/images/hero-desktop.jpg";
+  if (window.innerWidth <= 800) {
+    heroImage.setAttribute("src", mobileImage);
+  } else {
+    heroImage.setAttribute("src", desktopImage);
+  }
+}
+
+// if (condition) {
+//   statement;
+// }
+// console.log(window.innerWidth);
+window.addEventListener("resize", changeHeroByScreenSize);
+// Promise
+// Fetch API
+// DOM manipulation.
+// Loop
+// Arrays
+// Conditional Statement
+// Flex and Grid
